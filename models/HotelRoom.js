13909129e9d1570dfdefbb35017fa3d92d17af54
Mykcy4 <103573459+Mykcy4@users.noreply.github.com>
2024-11-24 23:38:09 +0100

    1

--- models/HotelRoom.js
+++ models/HotelRoom.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 // Connect to MongoDB
-const dbURI = "mongodb+srv://Mykcy:C7ugWr233KY9yhyr.r6xqr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const dbURI = "mongodb+srv://Mykcy:C7ugWr233KY9yhyr@cluster0.r6xqr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.connect(dbURI)
     .then(() => console.log('Connected to MongoDB'))
